d36a61f7575274ca88a1287d01e0584bbe0dff61
PavelVaavra <pavelvavra.vm@gmail.com>
2026-06-20 15:40:32 +0200

    Slog Groups

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"boot.dev/linko/internal/store"
+	pkgerr "github.com/pkg/errors"
 )
 
 func main() {
@@ -27,13 +28,22 @@ func main() {
 	os.Exit(status)
 }
 
+type stackTracer interface {
+	error
+	StackTrace() pkgerr.StackTrace
+}
+
 func replaceAttr(groups []string, a slog.Attr) slog.Attr {
 	if a.Key == "error" {
-		err, ok := a.Value.Any().(error)
-		if !ok {
-			return a
+		if stackErr, ok := errors.AsType[stackTracer](a.Value.Any().(error)); ok {
+			return slog.GroupAttrs("error", slog.Attr{
+				Key:   "message",
+				Value: slog.StringValue(stackErr.Error()),
+			}, slog.Attr{
+				Key:   "stack_trace",
+				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
+			})
 		}
-		return slog.String("error", fmt.Sprintf("%+v", err))
 	}
 	return a
 }
